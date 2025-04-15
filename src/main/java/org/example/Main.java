fa3f64fbbef65c20171828c7246b8e04cb3ba3ff
guustavomc <gustavomccb@gmail.com>
2025-04-14 22:14:25 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Order order = new Order("Gustavo","1","2","3");
+
+        System.out.println(order.getFlavor());
+    }
+}
---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,50 @@
+package org.example;
+
+
+
+public class Order {
+
+    private String customerName;
+    private String number;
+    private String flavor;
+    private String size;
+
+    public Order(String customerName, String number, String flavor, String size){
+        this.customerName=customerName;
+        this.number=number;
+        this.flavor=flavor;
+        this.size=size;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
